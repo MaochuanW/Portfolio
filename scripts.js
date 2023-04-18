458d4a2f4ce267098bc2cb3f5937cd16d051a559
MaochuanW <wangmaochuan@gmail.com>
2023-04-17 20:50:40 -0500

    update contact me

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    
+    fetch(event.target.action, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        },
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Your message has been sent!");
+            event.target.reset();
+        } else {
+            throw new Error("Something went wrong while submitting the form.");
+        }
+    })
+    .catch(error => {
+        alert("There was an error sending your message. Please try again later.");
+        console.error(error);
+    });
+});
